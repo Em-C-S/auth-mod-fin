12e3f466a5a542ce79430e55bcb4d6edea4e8c3b
Scott Lease <scottie_lease@hotmail.com>
2022-01-22 15:33:47 -0800

    files updated

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 require('dotenv').config();
-const server = require('./server.js');
-const { db } = require('./models/index.js');
+const server = require('./lib/server.js');
+const { db } = require('./lib/models');
 
 db.sync().then(() => {
   server.start(process.env.PORT || 3001);
